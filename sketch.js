a806a5f8443fcf5e54a41a4da700ac0b6330cfca
hostageDev <145352674+hostageDev@users.noreply.github.com>
2024-01-27 21:14:27 +0100

    added main files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+const canvasWidth = 1280;
+const canvasHeight = 720;
+
+function preload() {
+}
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    frameRate(60);
+
+    console.log("setup complete");
+}
+
+function draw() {
+}
